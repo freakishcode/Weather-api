1aab639162839224617983d0f68ce4d2189a40ce
Ajibola Bakare (aka Bolaji) <ajibolabakare@gmail.com>
2025-09-04 05:25:57 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,4 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
+  base: "/Weather-api", // 👈 must match GitHub Pages repo or subpath
 });
